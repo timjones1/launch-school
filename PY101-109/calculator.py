5cf3039416117442b3d4fb42ae4c7e0aa1d7c00d
Biggafish <timjones.gpu@gmail.com>
2024-09-20 23:10:59 +0100

    caculator bonus features

--- PY101-109/calculator.py
+++ PY101-109/calculator.py
@@ -11,40 +11,47 @@ def invalid_number(number_str):
 
     return False
 
-
 prompt('Welcome to Calculator!')
 
-prompt("What's the first number?")
-number1 = input()
+keep_calculating = True
 
-while invalid_number(number1):
-    prompt("this isnt a valid number:")
+while keep_calculating:
+
+    prompt("What's the first number?")
     number1 = input()
 
-prompt("What's the second number?")
-number2 = input()
+    while invalid_number(number1):
+        prompt("this isnt a valid number:")
+        number1 = input()
 
-while invalid_number(number2):
-    prompt("this isnt a valid number:")
+    prompt("What's the second number?")
     number2 = input()
 
-prompt('''What operation would you like to perform?\n
-       1) Add 2) Subtract 3) Multiply 4) Divide''')
-operation = input()
+    while invalid_number(number2):
+        prompt("this isnt a valid number:")
+        number2 = input()
 
-while operation not in ['1', '2', '3', '4']:
-    prompt("please enter 1, 2, 3 or 4")
+    prompt('''What operation would you like to perform?\n
+        1) Add 2) Subtract 3) Multiply 4) Divide''')
     operation = input()
 
-match operation:
+    while operation not in ['1', '2', '3', '4']:
+        prompt("please enter 1, 2, 3 or 4")
+        operation = input()
 
-    case '1':   # '1' represents addition
-        output = int(number1) + int(number2)
-    case '2': # '2' represents subtraction
-        output = int(number1) - int(number2)
-    case '3': # '3' represents multiplication
-        output = int(number1) * int(number2)
-    case '4': # '4' represents division
-        output = int(number1) / int(number2)
+    match operation:
 
-print(f"The result is: {output}")
+        case '1':   # '1' represents addition
+            output = int(number1) + int(number2)
+        case '2': # '2' represents subtraction
+            output = int(number1) - int(number2)
+        case '3': # '3' represents multiplication
+            output = int(number1) * int(number2)
+        case '4': # '4' represents division
+            output = int(number1) / int(number2)
+
+    print(f"The result is: {output}")
+
+    prompt('press y if you would like to complete another calculation, or any other key to finish:')
+
+    keep_calculating = (input() == "y")
