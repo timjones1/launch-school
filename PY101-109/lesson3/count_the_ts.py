790bd08e7e5184ccfd3de80d41278986d006db64
Biggafish <biggafish8@gmail.com>
2024-10-07 14:22:44 +0100

    simpler solution using built-in method

--- PY101-109/lesson3/count_the_ts.py
+++ PY101-109/lesson3/count_the_ts.py
@@ -1,4 +1,13 @@
+'''
+count the number of occurences of the letter 't' in a string.
+'''
+
 statement1 = "The Flintstones Rock!"
 statement2 = "Easy come, easy go."
 
-print(sum([1 for i in statement2 if i == 't']))
+print(sum([1 for i in statement1 if i == 't']))
+
+# again there is an easier built-in solution to this using a string method. this time it is .count().
+
+print(statement1.count('t'))
+print(statement2.count('t'))
