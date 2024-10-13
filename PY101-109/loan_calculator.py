e492b36f3f1872706d5e3b197a6c333282fe7f07
Biggafish <biggafish8@gmail.com>
2024-10-13 23:33:14 +0100

    math correction plus formatting of prompts

--- PY101-109/loan_calculator.py
+++ PY101-109/loan_calculator.py
@@ -1,13 +1,5 @@
-def validate_number(num):
-    
-    try:
-        float(num)
-    except ValueError:
-        return False
-    return True
-
-def validate_positive_number(num):
-    
+def valid_positive_number(num):
+    # check input num is a positive number that can be cast to float    
     try:
         float(num) 
     except ValueError:
@@ -15,30 +7,41 @@ def validate_positive_number(num):
     return float(num) >= 0
 
 def prompt(str_input):
+    # add a ==> to the str_input before printing
     print(f"==>{str_input}")
 
-loan_amount = input("please enter the loan amount in pounds and pence\
-e.g. 1000.00 \n")
 
-while not(validate_number(loan_amount)):
-    prompt("That is not a valid number, please enter a number e.g 500.00")
-    loan_amount = input()
+def main():
+    # main programme 
 
-apr_interest_rate = input("please enter the loan interest rate as a positive number, \
-for example for 5% enter 5, for 7.5% enter 7.5 \n")
+    loan_amount = input("please enter the loan amount in pounds and pence "\
+    "e.g. 1000.00 \n")
 
-while not(validate_positive_number(apr_interest_rate)):
-    prompt("This is not a valid interest rate, please enter a positive number")
-    apr_interest_rate = input()
+    while not(valid_positive_number(loan_amount)):
+        prompt("That is not a valid positive number, please try again:")
+        loan_amount = input()
 
-monthly_interest_rate = float(apr_interest_rate) / 12
+    apr_interest_rate = input("please enter the loan APR interest rate as a "\
+        "positive number \nfor example for 5% enter 5, for 7.5% enter 7.5: \n")
 
-loan_duration_months = input("please enter the duration of the loan in months, as a positive number \n")
+    while not(valid_positive_number(apr_interest_rate)):
+        prompt("This is not a valid interest rate, please enter a positive" \
+               " number:")
+        apr_interest_rate = input()
 
-while not(validate_positive_number(loan_duration_months)):
-    prompt("This is not a valid loan duration, please enter a positive number")
-    loan_duration_months = input()
+    monthly_interest_rate = float(apr_interest_rate) / 12 / 100
 
-monthly_repayment = float(loan_amount) * (monthly_interest_rate / (1 -(1 + monthly_interest_rate) **- float(loan_duration_months)))
+    loan_duration_months = input("please enter the duration of the loan in " \
+                                 "months, as a positive number \n")
 
-prompt(f"The monthly repayment amount is: £{"%.2f" % monthly_repayment}")
+    while not valid_positive_number(loan_duration_months):
+        prompt("This is not a valid loan duration, please enter a positive " \
+               "number \n")
+        loan_duration_months = input()
+
+    monthly_repayment = float(loan_amount) * (monthly_interest_rate / (1 -(1 + monthly_interest_rate) **- float(loan_duration_months)))
+
+    prompt(f"The monthly repayment amount is: £{'%.2f' % monthly_repayment}")
+
+
+main()
