a3708a8c693e90365c7c76b9ef3b152c5fbdb6ba
Biggafish <biggafish8@gmail.com>
2024-10-16 14:41:13 +0100

    fix linting issues, renamed local varibales

--- PY101-109/loan_calculator.py
+++ PY101-109/loan_calculator.py
@@ -20,7 +20,7 @@ def valid_duration(num):
         float(num)
     except ValueError:
         return False
-    
+
     if num in ["nan", "inf"]:
         return False
 
@@ -34,75 +34,75 @@ def prompt(str_input):
 
 def request_loan_amount():
     # request input of loan amount
-    loan_amount = input("Please enter the loan amount in pounds and pence "\
+    amount = input("Please enter the loan amount in pounds and pence "\
     "e.g. 1000.00 \n")
 
-    while not valid_number(loan_amount):
+    while not valid_number(amount):
         prompt("That is not a valid number, please try again:")
-        loan_amount = input()
-    
-    return float(loan_amount)
+        amount = input()
+
+    return float(amount)
 
 
 def request_interest_rate():
     # request input of interest rate to apply.
-    apr_interest_rate = input("Please enter the loan APR interest rate,"\
+    apr_rate = input("Please enter the loan APR interest rate,"\
         " for example for 5% enter 5,\nfor 7.5% enter 7.5: \n")
 
-    while not valid_number(apr_interest_rate):
+    while not valid_number(apr_rate):
         prompt("This is not a valid interest rate, please enter a positive" \
                " number:")
-        apr_interest_rate = input()
+        apr_rate = input()
 
-    monthly_interest_rate = float(apr_interest_rate) / 12 / 100
-    
-    return monthly_interest_rate
+    monthly_rate = float(apr_rate) / 12 / 100
+
+    return monthly_rate
 
 
 def request_loan_duration():
     # request input of loan duration with the option of years or months
-    loan_duration_months = input("Please enter the duration of the loan in " \
-    
+    duration_months = input("Please enter the duration of the loan in " \
+
                                  "months, or press y to enter in years: \n")
 
     # validate alternative duration in years and convert to months
-    if loan_duration_months.lower() == "y":
-        loan_duration_years = input("Please enter the duration of the loan " \
+    if duration_months.lower() == "y":
+        duration_years = input("Please enter the duration of the loan " \
                                  "in years: \n")
-        while not valid_duration(loan_duration_years):
+        while not valid_duration(duration_years):
             prompt("This is not a valid loan duration, please enter a " \
                "positive number")
-            loan_duration_years = input()
+            duration_years = input()
 
-        loan_duration_months = float(loan_duration_years) * 12
+        duration_months = float(duration_years) * 12
 
     # validate duration if entered in months
-    while not valid_duration(loan_duration_months):
+    while not valid_duration(duration_months):
         prompt("This is not a valid loan duration, please enter a positive " \
                "number")
-        loan_duration_months = input()
+        duration_months = input()
 
-    return float(loan_duration_months)
+    return float(duration_months)
 
-def calculate_monthly_repayment(loan_amount,monthly_interest_rate, 
-                                loan_duration_months):
+def calculate_monthly_repayment(amount,interest_rate,
+                                duration_months):
     # deal with possible zero interest rate value
-    if monthly_interest_rate > 0:
-        monthly_repayment = loan_amount * (monthly_interest_rate / \
-            (1 -(1 + monthly_interest_rate) ** -loan_duration_months))
+    if interest_rate > 0:
+        monthly_repayment = amount * (interest_rate / \
+            (1 -(1 + interest_rate) ** - duration_months))
     else:
-        monthly_repayment = loan_amount / float(loan_duration_months)
+        monthly_repayment = amount / float(duration_months)
 
     # return the calculated monthly repayment
-    prompt(f"The monthly repayment amount is: £{'%.2f' % monthly_repayment}")
+    prompt(f"The monthly repayment amount is: £{monthly_repayment:.2f}")
 
 
 while True:
     os.system('clear')
-    
+
     loan_amount = request_loan_amount()
     monthly_interest_rate = request_interest_rate()
-    loan_duration_months = request_loan_duration()   
+    loan_duration_months = request_loan_duration()
 
     calculate_monthly_repayment(loan_amount,monthly_interest_rate,
                                 loan_duration_months)
