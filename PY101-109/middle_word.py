8931a5d1e985637f23beb9ae598ac74d8cd5fdaa
Biggafish <biggafish8@gmail.com>
2024-10-23 00:25:15 +0100

    simplify conditional statements

--- PY101-109/middle_word.py
+++ PY101-109/middle_word.py
@@ -8,12 +8,12 @@ def middle_word(string):
     if len(words) == 1:
         return words[0]
 
+    middle = len(words) // 2 
+    
     if len(words) % 2 == 1:
-        middle = len(words) // 2 
         return words[middle]
     else:
-        middle = len(words) // 2 -1
-        return [words[middle],words[middle+1]]
+        return [words[middle-1], words[middle]]
 
 print(middle_word("last word before i sleep"))
 print(middle_word("one two three four"))
